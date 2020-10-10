4c78c0945ef5cd2032dd32dd2cf87d0d5b69378f
naoyasugita <sugita6878@gmail.com>
2020-10-10 20:33:09 +0900

    ニュースページのの大雑把な見た目を作成

--- clone_mrt_app/lib/screens/home_screen.dart
+++ clone_mrt_app/lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:clone_mrt_app/constants.dart';
 import 'package:clone_mrt_app/models/header_tab.dart';
+import 'package:clone_mrt_app/models/news.dart';
 import 'package:flutter/material.dart';
 
 class MyAppHome extends StatelessWidget {
@@ -58,22 +59,136 @@ class BuildAppBar extends StatelessWidget {
           ),
         ];
       },
-      body: TabBarView(
-        children: tabs.map((_tab) {
-          return SafeArea(
-            child: ListView.builder(
-              key: PageStorageKey(_tab.name),
-              itemCount: 30,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text('Item $index'),
-                );
-              },
-            ),
-          );
-        }).toList(),
-      ),
+      body: Body(),
     );
+  }
+}
+
+Container myContainer(String text) {
+  return Container(
+    alignment: Alignment.center,
+    width: 100,
+    height: 100,
+    child: Text(text),
+  );
+}
+
+class Body extends StatelessWidget {
+  const Body({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TabBarView(
+      children: [
+        NewsListView(),
+        myContainer("a"),
+        myContainer("a"),
+        myContainer("a"),
+        myContainer("a"),
+        myContainer("a"),
+        myContainer("a"),
+        myContainer("a"),
+        myContainer("a"),
+        myContainer("a"),
+        myContainer("a"),
+        myContainer("a"),
+        myContainer("a"),
+        myContainer("a"),
+        myContainer("a"),
+      ],
+    );
+  }
+}
+
+class NewsListView extends StatelessWidget {
+  const NewsListView({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      itemCount: newsList.length,
+      padding: EdgeInsets.all(4.0),
+      shrinkWrap: true,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        mainAxisSpacing: 10.0,
+        crossAxisSpacing: 10.0,
+        childAspectRatio: 0.7,
+      ),
+      itemBuilder: (context, index) {
+        return NewsView(index: index);
+      },
+    );
+  }
+}
+
+class NewsView extends StatelessWidget {
+  final index;
+  const NewsView({
+    Key key,
+    this.index,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            new BoxShadow(
+              color: Colors.grey,
+              offset: new Offset(0, 5.0),
+              blurRadius: 5.0,
+            )
+          ],
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.asset(
+              "assets/dummy_image.png",
+              fit: BoxFit.cover,
+            ),
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text(
+                newsList[index].title,
+                maxLines: 3,
+                overflow: TextOverflow.ellipsis,
+                style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8.0),
+              child: Text(
+                newsList[index].content,
+                maxLines: 3,
+                overflow: TextOverflow.ellipsis,
+                style: TextStyle(
+                  fontSize: 12,
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text(
+                newsList[index].timestamp,
+                style: TextStyle(
+                  fontSize: 10,
+                  color: Colors.grey,
+                ),
+              ),
+            ),
+          ],
+        ));
   }
 }
 
