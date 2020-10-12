b6904a25b2bfebe18f559789d62ae6eb9c10040e
naoyasugita <sugita6878@gmail.com>
2020-10-13 00:07:29 +0900

    画像のパスを定数に追加

--- clone_mrt_app/lib/constants.dart
+++ clone_mrt_app/lib/constants.dart
@@ -6,3 +6,7 @@ const kUnselectedLabelColor = Colors.black54;
 const kDefaultPadding = 8.0;
 
 const kBorderRadius = 4.0;
+
+const kWeatherSunny = "/assets/weather/sunny.png";
+const kWeatherCloudy = "/assets/weather/cloudy.png";
+const kWeatherRainy = "/assets/weather/rainy.png";
