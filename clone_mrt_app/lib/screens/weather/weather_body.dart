6ca4899120b617cd8187fb83d7637715829206dc
naoyasugita <sugita6878@gmail.com>
2020-10-15 21:23:57 +0900

    Widgetを細分化

--- clone_mrt_app/lib/screens/weather/weather_body.dart
+++ clone_mrt_app/lib/screens/weather/weather_body.dart
@@ -58,115 +58,12 @@ class _WeatherContent extends StatelessWidget {
       padding: EdgeInsets.all(kDefaultPadding),
       child: Column(
         children: [
-          Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-            Text(
-              recentWeather.locationName,
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Text(
-              recentWeather.updateDate,
-              style: TextStyle(
-                fontSize: 10,
-                color: Colors.grey,
-              ),
-            ),
-          ]),
+          WeatherContentHeader(recentWeather: recentWeather),
           Divider(
-            thickness: 0.5,
+            thickness: 0.3,
             color: Colors.grey,
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Column(
-                children: [
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 20),
-                    child: RichText(
-                      text: TextSpan(
-                        children: [
-                          TextSpan(
-                            text: 'きょう',
-                            style: TextStyle(
-                              fontSize: 24,
-                              color: Colors.greenAccent[700],
-                            ),
-                          ),
-                          TextSpan(text: "\n"),
-                          TextSpan(
-                            text: recentWeather.datetime,
-                            style: TextStyle(
-                              fontSize: 16,
-                              color: Colors.black,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 30),
-                child: Image.asset(
-                  recentWeather.weatherIconPath,
-                  fit: BoxFit.cover,
-                  width: 40,
-                ),
-              ),
-              Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        recentWeather.highestTemperature.toString(),
-                        style: TextStyle(
-                          fontSize: 24,
-                          color: Colors.red[400],
-                        ),
-                      ),
-                      Text(
-                        recentWeather.lowestTemperature.toString(),
-                        style: TextStyle(
-                          fontSize: 24,
-                          color: Colors.blue,
-                        ),
-                      ),
-                      Text("℃"),
-                    ],
-                  ),
-                  Row(
-                    // crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      rainyPercentText(recentWeather.rainyPercent[0]),
-                      rainyPercentText(recentWeather.rainyPercent[1]),
-                      rainyPercentText(recentWeather.rainyPercent[2]),
-                      rainyPercentText(recentWeather.rainyPercent[3]),
-                      Text("％"),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      smallGrayText("0"),
-                      smallGrayText("-"),
-                      smallGrayText("6"),
-                      smallGrayText("-"),
-                      smallGrayText("12"),
-                      smallGrayText("-"),
-                      smallGrayText("18"),
-                      smallGrayText("-"),
-                      smallGrayText("24"),
-                    ],
-                  ),
-                ],
-              ),
-            ],
-          ),
+          WeatherContentBody(recentWeather: recentWeather),
         ],
       ),
       alignment: Alignment.center,
@@ -181,6 +78,88 @@ class _WeatherContent extends StatelessWidget {
           )
         ],
       ),
+    );
+  }
+}
+
+class WeatherContentBody extends StatelessWidget {
+  const WeatherContentBody({
+    Key key,
+    @required this.recentWeather,
+  }) : super(key: key);
+
+  final WeatherModel recentWeather;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        WeatherContentDateInfo(recentWeather: recentWeather),
+        WeatherContentIcon(recentWeather: recentWeather),
+        WeatherContentDetail(recentWeather: recentWeather),
+      ],
+    );
+  }
+}
+
+class WeatherContentDetail extends StatelessWidget {
+  const WeatherContentDetail({
+    Key key,
+    @required this.recentWeather,
+  }) : super(key: key);
+
+  final WeatherModel recentWeather;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              recentWeather.highestTemperature.toString(),
+              style: TextStyle(
+                fontSize: 24,
+                color: Colors.red[400],
+              ),
+            ),
+            Text(
+              recentWeather.lowestTemperature.toString(),
+              style: TextStyle(
+                fontSize: 24,
+                color: Colors.blue,
+              ),
+            ),
+            Text("℃"),
+          ],
+        ),
+        Row(
+          // crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            rainyPercentText(recentWeather.rainyPercent[0]),
+            rainyPercentText(recentWeather.rainyPercent[1]),
+            rainyPercentText(recentWeather.rainyPercent[2]),
+            rainyPercentText(recentWeather.rainyPercent[3]),
+            Text("％"),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            smallGrayText("0"),
+            smallGrayText("-"),
+            smallGrayText("6"),
+            smallGrayText("-"),
+            smallGrayText("12"),
+            smallGrayText("-"),
+            smallGrayText("18"),
+            smallGrayText("-"),
+            smallGrayText("24"),
+          ],
+        ),
+      ],
     );
   }
 
@@ -199,4 +178,95 @@ class _WeatherContent extends StatelessWidget {
           ),
         ),
       );
+}
+
+class WeatherContentIcon extends StatelessWidget {
+  const WeatherContentIcon({
+    Key key,
+    @required this.recentWeather,
+  }) : super(key: key);
+
+  final WeatherModel recentWeather;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 30),
+      child: Image.asset(
+        recentWeather.weatherIconPath,
+        fit: BoxFit.cover,
+        width: 40,
+      ),
+    );
+  }
+}
+
+class WeatherContentDateInfo extends StatelessWidget {
+  const WeatherContentDateInfo({
+    Key key,
+    @required this.recentWeather,
+  }) : super(key: key);
+
+  final WeatherModel recentWeather;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          child: RichText(
+            text: TextSpan(
+              children: [
+                TextSpan(
+                  text: 'きょう',
+                  style: TextStyle(
+                    fontSize: 24,
+                    color: Colors.greenAccent[700],
+                  ),
+                ),
+                TextSpan(text: "\n"),
+                TextSpan(
+                  text: recentWeather.datetime,
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class WeatherContentHeader extends StatelessWidget {
+  const WeatherContentHeader({
+    Key key,
+    @required this.recentWeather,
+  }) : super(key: key);
+
+  final WeatherModel recentWeather;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+      Text(
+        recentWeather.locationName,
+        style: TextStyle(
+          fontSize: 16,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      Text(
+        recentWeather.updateDate,
+        style: TextStyle(
+          fontSize: 10,
+          color: Colors.grey,
+        ),
+      ),
+    ]);
+  }
 }
