43642dfec336c2390e8a17c428029633259cdcfc
naoyasugita <sugita6878@gmail.com>
2020-10-17 02:28:08 +0900

    モデルの追加

--- clone_mrt_app/lib/models/weather.dart
+++ clone_mrt_app/lib/models/weather.dart
@@ -27,12 +27,69 @@ class WeatherModel {
   });
 }
 
+List<WeatherModel> weaklyWeatherList = [
+  WeatherModel(
+    locationName: "宮崎",
+    weatherIconPath: kWeatherSunny,
+    datetime: "12日(火)",
+    highestTemperature: 30,
+    lowestTemperature: 21,
+    rainyPercent: ["30", "20", "0", "30"],
+    updateDate: "2020/10/11 13:30更新",
+  ),
+  WeatherModel(
+    locationName: "宮崎",
+    weatherIconPath: kWeatherCloudy,
+    datetime: "13日(水)",
+    highestTemperature: 31,
+    lowestTemperature: 19,
+    rainyPercent: ["30", "20", "0", "30"],
+    updateDate: "2020/10/11 13:30更新",
+  ),
+  WeatherModel(
+    locationName: "宮崎",
+    weatherIconPath: kWeatherCloudy,
+    datetime: "14日(木)",
+    highestTemperature: 30,
+    lowestTemperature: 23,
+    rainyPercent: ["30", "20", "0", "30"],
+    updateDate: "2020/10/11 13:30更新",
+  ),
+  WeatherModel(
+    locationName: "宮崎",
+    weatherIconPath: kWeatherRainy,
+    datetime: "15日(金)",
+    highestTemperature: 31,
+    lowestTemperature: 21,
+    rainyPercent: ["30", "20", "0", "30"],
+    updateDate: "2020/10/11 13:30更新",
+  ),
+  WeatherModel(
+    locationName: "宮崎",
+    weatherIconPath: kWeatherSunny,
+    datetime: "16日(土)",
+    highestTemperature: 30,
+    lowestTemperature: 20,
+    rainyPercent: ["30", "20", "0", "30"],
+    updateDate: "2020/10/11 13:30更新",
+  ),
+  WeatherModel(
+    locationName: "宮崎",
+    weatherIconPath: kWeatherSunny,
+    datetime: "17日(日)",
+    highestTemperature: 29,
+    lowestTemperature: 21,
+    rainyPercent: ["30", "20", "0", "30"],
+    updateDate: "2020/10/11 13:30更新",
+  ),
+];
+
 WeatherModel recentWeather = WeatherModel(
   locationName: "宮崎",
-  weatherIconPath: kWeatherSunny,
-  datetime: "12日(月)",
-  highestTemperature: 30,
-  lowestTemperature: 20,
+  weatherIconPath: kWeatherRainy,
+  datetime: "11日(月)",
+  highestTemperature: 27,
+  lowestTemperature: 19,
   rainyPercent: ["30", "20", "0", "30"],
   updateDate: "2020/10/11 13:30更新",
 );
