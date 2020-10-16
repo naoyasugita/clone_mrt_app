883ff912d80610304db6454bd7fc0a32b088e0a9
naoyasugita <sugita6878@gmail.com>
2020-10-17 02:31:57 +0900

    週間天気を作成

--- clone_mrt_app/lib/screens/weather/weather_body.dart
+++ clone_mrt_app/lib/screens/weather/weather_body.dart
@@ -10,7 +10,7 @@ const smallCard = StaggeredTile.count(1, 0.7);
 
 List<StaggeredTile> _staggeredTiles = <StaggeredTile>[
   middleCard,
-  // middleCard,
+  middleCard,
   // smallCard,
   // smallCard,
   // largeCard,
@@ -22,8 +22,115 @@ List<StaggeredTile> _staggeredTiles = <StaggeredTile>[
 ];
 
 List<Widget> _tiles = <Widget>[
-  WeatherContent(recentWeather),
-  // WeatherContent(recentWeather),
+  Padding(
+    padding: EdgeInsets.all(4),
+    child: WeatherContent(recentWeather),
+  ),
+  Padding(
+    padding: EdgeInsets.all(4),
+    child: Container(
+      padding: EdgeInsets.all(8),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "週間天気",
+            style: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          // TODO: 区切り線の追加
+          Container(
+            padding: EdgeInsets.only(top: 2),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: List.generate(
+                weaklyWeatherList.length,
+                (index) => Container(
+                  padding: EdgeInsets.symmetric(horizontal: 4),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(
+                          bottom: 4,
+                        ),
+                        child: Text(
+                          weaklyWeatherList[index].datetime,
+                          style: TextStyle(
+                            fontSize: 10,
+                            color: _checkWeekend(
+                              weaklyWeatherList[index].datetime,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(
+                          bottom: 4,
+                        ),
+                        child: Image.asset(
+                          weaklyWeatherList[index].weatherIconPath,
+                          width: 24,
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(
+                          bottom: 4,
+                        ),
+                        child: Text(
+                          weaklyWeatherList[index]
+                              .highestTemperature
+                              .toString(),
+                          style: TextStyle(
+                            fontSize: 10,
+                            color: Colors.red[400],
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(
+                          bottom: 4,
+                        ),
+                        child: Text(
+                          weaklyWeatherList[index].lowestTemperature.toString(),
+                          style: TextStyle(
+                            fontSize: 10,
+                            color: Colors.blue,
+                          ),
+                        ),
+                      ),
+                      Text(
+                        weaklyWeatherList[index].rainyPercent[0] + "%",
+                        style: TextStyle(
+                          fontSize: 10,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            // WeatherContentHeader(recentWeather: recentWeather),
+            // WeatherContentBody(recentWeather: recentWeather),
+          ),
+        ],
+      ),
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(kBorderRadius),
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey,
+            offset: new Offset(0, 5.0),
+            blurRadius: 5.0,
+          )
+        ],
+      ),
+    ),
+  ),
   // WeatherContent(recentWeather),
   // WeatherContent(recentWeather),
   // WeatherContent(recentWeather),
@@ -34,11 +141,20 @@ List<Widget> _tiles = <Widget>[
   // WeatherContent(recentWeather),
 ];
 
+_checkWeekend(String datetime) {
+  if (datetime.contains("土")) {
+    return Colors.blue;
+  } else if (datetime.contains("(日)")) {
+    return Colors.red[400];
+  }
+  return;
+}
+
 class WeatherView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return StaggeredGridView.count(
-      padding: EdgeInsets.all(kDefaultPadding),
+      padding: EdgeInsets.all(4),
       crossAxisCount: 2,
       staggeredTiles: _staggeredTiles,
       children: _tiles,
