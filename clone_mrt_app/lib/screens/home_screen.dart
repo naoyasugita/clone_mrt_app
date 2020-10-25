75af67f2231298551b3f069025d59cc83a0c6b63
naoya_s <sugita6878@gmail.com>
2020-10-25 23:35:49 +0900

    Revert "速報履歴詳細ページの作成"

--- clone_mrt_app/lib/screens/home_screen.dart
+++ clone_mrt_app/lib/screens/home_screen.dart
@@ -1,5 +1,4 @@
 import 'package:clone_mrt_app/constants.dart';
-import 'package:clone_mrt_app/models/breaking_news.dart';
 import 'package:clone_mrt_app/models/header_tab.dart';
 import 'package:clone_mrt_app/screens/event/event_body.dart';
 import 'package:clone_mrt_app/screens/holiday_doctor.dart/holiday_doctor_body.dart';
@@ -91,7 +90,7 @@ class Body extends StatelessWidget {
       children: [
         NewsListView(),
         WeatherView(),
-        BreakingNewsView(),
+        myContainer("a"),
         myContainer("a"),
         myContainer("a"),
         myContainer("a"),
@@ -105,125 +104,6 @@ class Body extends StatelessWidget {
         LiveCameraListView(),
         UsageListView(),
       ],
-    );
-  }
-}
-
-class BreakingNewsView extends StatelessWidget {
-  const BreakingNewsView({
-    Key key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    final Size size = MediaQuery.of(context).size;
-
-    return Container(
-      child: ListView.builder(
-        itemCount: breakingNewsList.length,
-        itemBuilder: (context, index) {
-          return GestureDetector(
-            onTap: () {
-              print(size.width);
-            },
-            child: Container(
-              child: Column(
-                children: [
-                  Padding(
-                    padding: EdgeInsets.symmetric(vertical: 10),
-                    child: Text(
-                      breakingNewsList[index].datetime.getMonthDay(),
-                      style: TextStyle(
-                        fontSize: 16,
-                      ),
-                    ),
-                  ),
-                  Row(
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 5),
-                        child: Text(
-                          breakingNewsList[index].datetime.getHourMinutes(),
-                        ),
-                      ),
-                      Container(
-                        // width: 340,
-                        alignment: Alignment.center,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(kBorderRadius),
-                          color: Colors.white,
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey[400],
-                              offset: Offset(0, 3.0),
-                              blurRadius: 1.0,
-                            )
-                          ],
-                        ),
-                        child: BreakingNewsContents(index: index),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          );
-        },
-      ),
-    );
-  }
-}
-
-class BreakingNewsContents extends StatelessWidget {
-  const BreakingNewsContents({
-    Key key,
-    @required this.index,
-  }) : super(key: key);
-
-  final index;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Row(
-        children: [
-          Padding(
-            padding: EdgeInsets.all(5),
-            child: Icon(
-              breakingNewsList[index].categpry.toIcon(),
-              size: 50,
-            ),
-          ),
-          // TODO: 横幅を調整できるようにする
-          // https://itome.team/blog/2019/12/flutter-advent-calendar-day9/
-          Container(
-            padding: EdgeInsets.only(right: 10),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  breakingNewsList[index].title,
-                  overflow: TextOverflow.ellipsis,
-                  softWrap: true,
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 15,
-                  ),
-                ),
-                Text(
-                  breakingNewsList[index].content,
-                  maxLines: 1,
-                  softWrap: false,
-                  overflow: TextOverflow.ellipsis,
-                  style: TextStyle(
-                    fontSize: 12,
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
     );
   }
 }
