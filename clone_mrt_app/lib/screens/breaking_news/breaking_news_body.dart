8c1b3d12c53fd120218eb49ba0c5039543126307
naoyasugita <sugita6878@gmail.com>
2020-10-25 23:31:48 +0900

    ファイルを分割した

---
+++ clone_mrt_app/lib/screens/breaking_news/breaking_news_body.dart
@@ -0,0 +1,122 @@
+import 'package:clone_mrt_app/constants.dart';
+import 'package:clone_mrt_app/models/breaking_news.dart';
+import 'package:flutter/material.dart';
+
+class BreakingNewsView extends StatelessWidget {
+  const BreakingNewsView({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+
+    return ListView.builder(
+      itemCount: breakingNewsList.length,
+      itemBuilder: (context, index) {
+        return GestureDetector(
+          onTap: () {
+            print(size.width);
+          },
+          child: Container(
+            child: Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 10),
+                  child: Text(
+                    breakingNewsList[index].datetime.getMonthDay(),
+                    style: TextStyle(
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+                Row(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 5),
+                      child: Text(
+                        breakingNewsList[index].datetime.getHourMinutes(),
+                      ),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(kBorderRadius),
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey[400],
+                            offset: Offset(0, 3.0),
+                            blurRadius: 1.0,
+                          )
+                        ],
+                      ),
+                      child: BreakingNewsContents(index: index, size: size),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+      // ),
+    );
+  }
+}
+
+class BreakingNewsContents extends StatelessWidget {
+  const BreakingNewsContents({
+    Key key,
+    @required this.index,
+    this.size,
+  }) : super(key: key);
+
+  final index;
+  final size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      width: size.width - 60,
+      child: Row(
+        children: [
+          Icon(
+            breakingNewsList[index].categpry.toIcon(),
+            size: 50,
+          ),
+          // TODO: 横幅を調整できるようにする
+          // https://itome.team/blog/2019/12/flutter-advent-calendar-day9/
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.only(left: 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    breakingNewsList[index].title,
+                    overflow: TextOverflow.ellipsis,
+                    softWrap: true,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15,
+                    ),
+                  ),
+                  Text(
+                    breakingNewsList[index].content,
+                    maxLines: 3,
+                    softWrap: true,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(
+                      fontSize: 12,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
