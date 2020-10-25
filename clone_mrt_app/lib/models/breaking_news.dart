4abb40501583ae62e1c0398d1336a81e4837548f
naoyasugita <sugita6878@gmail.com>
2020-10-25 23:32:15 +0900

    モデルを修正

--- clone_mrt_app/lib/models/breaking_news.dart
+++ clone_mrt_app/lib/models/breaking_news.dart
@@ -71,37 +71,37 @@ List<BreakingNewsModel> breakingNewsList = [
   BreakingNewsModel(
     title: shortTitle,
     categpry: BreakinngNewsCategoty(NewsCategpryType.Info),
-    datetime: BreakingNewsDatatime("10月11日(月) 18:30"),
+    datetime: BreakingNewsDatatime("10月20日(月) 18:30"),
     content: contentText,
   ),
   BreakingNewsModel(
     title: shortTitle,
     categpry: BreakinngNewsCategoty(NewsCategpryType.Info),
-    datetime: BreakingNewsDatatime("10月11日(月) 18:30"),
-    content: contentText,
+    datetime: BreakingNewsDatatime("10月16日(金) 11:30"),
+    content: shortTitle,
   ),
   BreakingNewsModel(
     title: "震源・震度情報",
     categpry: BreakinngNewsCategoty(NewsCategpryType.Earthquake),
-    datetime: BreakingNewsDatatime("10月11日(月) 18:30"),
+    datetime: BreakingNewsDatatime("10月14日(水) 10:01"),
     content: contentText,
   ),
   BreakingNewsModel(
     title: "警報・注意報",
     categpry: BreakinngNewsCategoty(NewsCategpryType.Caution),
-    datetime: BreakingNewsDatatime("10月11日(月) 18:30"),
+    datetime: BreakingNewsDatatime("10月12日(月) 20:51"),
     content: contentText,
   ),
   BreakingNewsModel(
     title: "ニュース速報",
     categpry: BreakinngNewsCategoty(NewsCategpryType.News),
-    datetime: BreakingNewsDatatime("10月11日(月) 18:30"),
+    datetime: BreakingNewsDatatime("10月11日(日) 13:00"),
     content: contentText,
   ),
 ];
 
 String contentText =
-    "親譲りの無鉄砲で小供の時から損ばかりしている。親譲りの無鉄砲で小供の時から損ばかりしている。親譲りの無鉄砲で小供の時から損ばかりしている。";
+    "親譲りの無鉄砲で子供の時から損ばかりしている。小学校に居る時分学校の二階から飛び降りて一週間程腰を抜かした事がある。なぜそんな無闇（むやみ）をしたと聞く人があるかもしれぬ。別段深い理由でもない。";
 
 String longTitle = "テストテストテストテストテストテストテストテストテストテストテストテストテストテストテスト";
 
