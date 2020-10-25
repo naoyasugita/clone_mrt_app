082b75e5795c2f28c62252ce8fa5ebe715b11ab4
naoyasugita <sugita6878@gmail.com>
2020-10-25 21:51:39 +0900

    ファイルを分割した

--- clone_mrt_app/lib/screens/home_screen.dart
+++ clone_mrt_app/lib/screens/home_screen.dart
@@ -1,15 +1,13 @@
 import 'package:clone_mrt_app/constants.dart';
-import 'package:clone_mrt_app/models/event.dart';
 import 'package:clone_mrt_app/models/header_tab.dart';
-import 'package:clone_mrt_app/models/holiday_doctor.dart';
 import 'package:clone_mrt_app/screens/event/event_body.dart';
+import 'package:clone_mrt_app/screens/holiday_doctor.dart/holiday_doctor_body.dart';
 import 'package:clone_mrt_app/screens/live_camera/live_camera_body.dart';
 import 'package:clone_mrt_app/screens/news/news_body.dart';
 import 'package:clone_mrt_app/screens/uchinoko/uchinoko_body.dart';
 import 'package:clone_mrt_app/screens/usage/usage_body.dart';
 import 'package:clone_mrt_app/screens/weather/weather_body.dart';
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 class MyAppHome extends StatelessWidget {
   const MyAppHome();
@@ -102,102 +100,10 @@ class Body extends StatelessWidget {
         UchinokoListView(),
         myContainer("a"),
         EventListView(),
-        ExpandableItemExample(),
+        HospitalListView(),
         LiveCameraListView(),
         UsageListView(),
       ],
-    );
-  }
-}
-
-class ExpandableItemExample extends StatelessWidget {
-  final List<HospitalInfoModelList> _hospitalList = hospitalList;
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      itemBuilder: (BuildContext context, int index) => Container(
-        margin: EdgeInsets.only(
-          right: 5,
-          bottom: 5,
-          left: 5,
-        ),
-        decoration: BoxDecoration(
-          color: Colors.purple[100],
-          borderRadius: BorderRadius.circular(kBorderRadius),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey,
-              offset: Offset(0, 5.0),
-              blurRadius: 5.0,
-            )
-          ],
-        ),
-        child: HospitalInfoView(hospitalList: _hospitalList, index: index),
-      ),
-      itemCount: _hospitalList.length,
-    );
-  }
-}
-
-class HospitalInfoView extends StatelessWidget {
-  final index;
-
-  const HospitalInfoView({
-    Key key,
-    this.index,
-    @required List<HospitalInfoModelList> hospitalList,
-  })  : _hospitalList = hospitalList,
-        super(key: key);
-
-  final List<HospitalInfoModelList> _hospitalList;
-
-  @override
-  Widget build(BuildContext context) {
-    return ExpansionTile(
-      backgroundColor: Colors.purple[100],
-      title: Text(_hospitalList[index].hospitalType.toJapanese()),
-      leading: Icon(Icons.add_box),
-      children: buildList(index).toList(),
-    );
-  }
-
-  List<Container> buildList(int index) {
-    return List.generate(
-      _hospitalList[index].hospitalList.length,
-      (_index) => Container(
-        padding: EdgeInsets.all(10),
-        alignment: Alignment.centerLeft,
-        margin: EdgeInsets.only(
-          right: 10,
-          bottom: 10,
-          left: 10,
-        ),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(kBorderRadius),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey,
-              offset: Offset(0, 5.0),
-              blurRadius: 5.0,
-            )
-          ],
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(_hospitalList[index].hospitalList[_index].datetime +
-                _hospitalList[index].hospitalList[_index].cityName),
-            Text(
-              _hospitalList[index].hospitalList[_index].hospitalName,
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
