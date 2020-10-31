e7772c7b567901f960e411df2ecc335c38e95c35
naoyasugita <sugita6878@gmail.com>
2020-10-31 22:45:31 +0900

    ファイルを分割

--- clone_mrt_app/lib/screens/home_screen.dart
+++ clone_mrt_app/lib/screens/home_screen.dart
@@ -1,6 +1,5 @@
 import 'package:clone_mrt_app/constants.dart';
 import 'package:clone_mrt_app/models/header_tab.dart';
-import 'package:clone_mrt_app/models/tv.dart';
 import 'package:clone_mrt_app/screens/apply_post/apply_post_body.dart';
 import 'package:clone_mrt_app/screens/broadcast/broadcast_body.dart';
 import 'package:clone_mrt_app/screens/event/event_body.dart';
@@ -11,11 +10,11 @@ import 'package:clone_mrt_app/screens/news/news_body.dart';
 import 'package:clone_mrt_app/screens/overcome/overcome_body.dart';
 import 'package:clone_mrt_app/screens/present/present_body.dart';
 import 'package:clone_mrt_app/screens/shop_introduction/shop_introduction.dart';
+import 'package:clone_mrt_app/screens/tv/tv_body.dart';
 import 'package:clone_mrt_app/screens/uchinoko/uchinoko_body.dart';
 import 'package:clone_mrt_app/screens/usage/usage_body.dart';
 import 'package:clone_mrt_app/screens/weather/weather_body.dart';
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 class MyAppHome extends StatelessWidget {
   const MyAppHome();
@@ -113,116 +112,6 @@ class Body extends StatelessWidget {
         LiveCameraListView(),
         UsageListView(),
       ],
-    );
-  }
-}
-
-class TVView extends StatelessWidget {
-  const TVView({
-    Key key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: SingleChildScrollView(
-        child: Column(
-          children: [
-            TVHeader(),
-            TVProgramList(),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class TVProgramList extends StatelessWidget {
-  const TVProgramList({
-    Key key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      // TODO: 一部のリストはWidgetで作成する
-      shrinkWrap: true,
-      physics: NeverScrollableScrollPhysics(),
-      itemCount: tvList.length,
-      itemBuilder: (context, index) {
-        return GestureDetector(
-          onTap: () {
-            _launchURL(tvList[index].url);
-          },
-          child: Container(
-            width: 300,
-            margin: EdgeInsets.only(
-              top: 10,
-              right: 10,
-              left: 10,
-            ),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(kBorderRadius),
-              color: Colors.white,
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey,
-                  offset: Offset(0, 5.0),
-                  blurRadius: 5.0,
-                ),
-              ],
-            ),
-            child: ClipRRect(
-              borderRadius: BorderRadius.all(
-                Radius.circular(kBorderRadius),
-              ),
-              child: Image.asset(
-                tvList[index].imagePath,
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-        );
-      },
-    );
-  }
-
-  _launchURL(String url) async {
-    if (await canLaunch(url)) {
-      await launch(
-        url,
-        forceSafariVC: true,
-        forceWebView: true,
-      );
-    } else {
-      throw 'Could not launch $url';
-    }
-  }
-}
-
-class TVHeader extends StatelessWidget {
-  const TVHeader({
-    Key key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      margin: EdgeInsets.only(
-        top: 10,
-        right: 10,
-        left: 10,
-      ),
-      child: ClipRRect(
-        borderRadius: BorderRadius.all(
-          Radius.circular(kBorderRadius),
-        ),
-        child: Image.asset(
-          "assets/tv_top.png",
-          fit: BoxFit.fitWidth,
-        ),
-      ),
     );
   }
 }
