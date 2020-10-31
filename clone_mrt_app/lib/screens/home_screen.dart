1a3ff3ebf062b59099e2b89c73f68e649074d68b
naoyasugita <sugita6878@gmail.com>
2020-10-31 15:21:17 +0900

    グリッド表示部分の作成

--- clone_mrt_app/lib/screens/home_screen.dart
+++ clone_mrt_app/lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:clone_mrt_app/constants.dart';
 import 'package:clone_mrt_app/models/header_tab.dart';
 import 'package:clone_mrt_app/models/overcome.dart';
+import 'package:clone_mrt_app/models/uchinoko.dart';
 import 'package:clone_mrt_app/screens/apply_post/apply_post_body.dart';
 import 'package:clone_mrt_app/screens/broadcast/broadcast_body.dart';
 import 'package:clone_mrt_app/screens/event/event_body.dart';
@@ -130,10 +131,105 @@ class OvercomeView extends StatelessWidget {
           children: [
             OvercomeListView(),
             TsunagoHeader(),
+            TsunagoBody(),
           ],
         ),
       ),
     );
+  }
+}
+
+class TsunagoBody extends StatelessWidget {
+  const TsunagoBody({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      itemCount: uchinokoList.length,
+      padding: EdgeInsets.all(kDefaultPadding),
+      shrinkWrap: true,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        mainAxisSpacing: 10.0,
+        crossAxisSpacing: 10.0,
+        childAspectRatio: 0.8,
+      ),
+      itemBuilder: (context, index) {
+        return GestureDetector(
+          onTap: () {
+            _launchURL(tsunagouList[index].youtubeUrl);
+          },
+          child: Container(
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(kBorderRadius),
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey,
+                  offset: Offset(0, 5.0),
+                  blurRadius: 5.0,
+                )
+              ],
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              // TODO: 画像の角を直接丸くするのをやめて、親ウィジェットをに合わせる
+              children: [
+                Center(
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.vertical(
+                      top: Radius.circular(kBorderRadius),
+                    ),
+                    child: Image.asset(
+                      tsunagouList[index].thumbnailPath,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(kDefaultPadding),
+                  child: Text(
+                    tsunagouList[index].title,
+                    maxLines: 3,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                  child: Text(
+                    tsunagouList[index].content,
+                    maxLines: 3,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(
+                      fontSize: 12,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  _launchURL(String url) async {
+    if (await canLaunch(url)) {
+      await launch(
+        url,
+        forceSafariVC: true,
+        forceWebView: true,
+      );
+    } else {
+      throw 'Could not launch $url';
+    }
   }
 }
 
