92a74a9ed7706e60e90e2e9ac5f49ae0471e5b8f
naoyasugita <sugita6878@gmail.com>
2020-10-31 15:01:29 +0900

    Overcomeモデルを作成

---
+++ clone_mrt_app/lib/models/overcome.dart
@@ -0,0 +1,61 @@
+class OvercomeModel {
+  final String title;
+  final String imagePath;
+  final String url;
+
+  OvercomeModel({
+    this.title,
+    this.imagePath,
+    this.url,
+  });
+}
+
+List<OvercomeModel> overcomeModelList = [
+  OvercomeModel(
+    title: "コロナ",
+    imagePath: "assets/dummy_broadcast.png",
+    url: "https://mrt.jp/special/coronavirus",
+  ),
+  OvercomeModel(
+    title: "新しい生活様式",
+    imagePath: "assets/dummy_broadcast.png",
+    url: "https://app.mrt.jp/contents/newlifestyle/newlifestyle.html",
+  ),
+  OvercomeModel(
+    title: "COCOA",
+    imagePath: "assets/dummy_broadcast.png",
+    url: "https://www.mhlw.go.jp/stf/seisakunitsuite/bunya/cocoa_00138.html",
+  ),
+  OvercomeModel(
+    title: "つなごうみやざき",
+    imagePath: "assets/dummy_broadcast.png",
+    url: "https://mrt.jp/event/?id=tsunagomiyazaki",
+  ),
+  OvercomeModel(
+    title: "感知るみやざき",
+    imagePath: "assets/dummy_broadcast.png",
+    url: "https://www.kanko-miyazaki.jp/kanjirumiyazaki/",
+  ),
+  OvercomeModel(
+    title: "TAKEOUT",
+    imagePath: "assets/dummy_broadcast.png",
+    url: "https://www.miyazaki-city.tourism.or.jp/feature/takeout-and-delivery",
+  ),
+  OvercomeModel(
+    title: "みやざきとともに",
+    imagePath: "assets/dummy_broadcast.png",
+    url: "https://mrt.jp/special/miyazaki_to_tomoni/",
+  ),
+  OvercomeModel(
+    title: "県内の感染状況(警戒レベル)及び注意すべき県外の地域",
+    imagePath: "assets/dummy_broadcast.png",
+    url:
+        "https://www.pref.miyazaki.lg.jp/kansensho-taisaku/covid-19/yobo/hassei.html",
+  ),
+  OvercomeModel(
+    title: "宮崎県における感染者状況一覧",
+    imagePath: "assets/dummy_broadcast.png",
+    url:
+        "https://www.pref.miyazaki.lg.jp/kansensho-taisaku/covid-19/hassei_list.html",
+  ),
+];
