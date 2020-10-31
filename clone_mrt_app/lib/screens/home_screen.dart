dfc328b79bd0739121c89315ce4dfdfa982a888b
naoyasugita <sugita6878@gmail.com>
2020-10-31 15:23:52 +0900

    ファイルを分割

--- clone_mrt_app/lib/screens/home_screen.dart
+++ clone_mrt_app/lib/screens/home_screen.dart
@@ -1,7 +1,5 @@
 import 'package:clone_mrt_app/constants.dart';
 import 'package:clone_mrt_app/models/header_tab.dart';
-import 'package:clone_mrt_app/models/overcome.dart';
-import 'package:clone_mrt_app/models/uchinoko.dart';
 import 'package:clone_mrt_app/screens/apply_post/apply_post_body.dart';
 import 'package:clone_mrt_app/screens/broadcast/broadcast_body.dart';
 import 'package:clone_mrt_app/screens/event/event_body.dart';
@@ -9,15 +7,13 @@ import 'package:clone_mrt_app/screens/breaking_news/breaking_news_body.dart';
 import 'package:clone_mrt_app/screens/holiday_doctor/holiday_doctor_body.dart';
 import 'package:clone_mrt_app/screens/live_camera/live_camera_body.dart';
 import 'package:clone_mrt_app/screens/news/news_body.dart';
+import 'package:clone_mrt_app/screens/overcome/overcome_body.dart';
 import 'package:clone_mrt_app/screens/present/present_body.dart';
 import 'package:clone_mrt_app/screens/shop_introduction/shop_introduction.dart';
 import 'package:clone_mrt_app/screens/uchinoko/uchinoko_body.dart';
 import 'package:clone_mrt_app/screens/usage/usage_body.dart';
 import 'package:clone_mrt_app/screens/weather/weather_body.dart';
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
-
-import 'package:gradient_text/gradient_text.dart';
 
 class MyAppHome extends StatelessWidget {
   const MyAppHome();
@@ -114,234 +110,6 @@ class Body extends StatelessWidget {
         LiveCameraListView(),
         UsageListView(),
       ],
-    );
-  }
-}
-
-class OvercomeView extends StatelessWidget {
-  const OvercomeView({
-    Key key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: SingleChildScrollView(
-        child: Column(
-          children: [
-            OvercomeListView(),
-            TsunagoHeader(),
-            TsunagoBody(),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class TsunagoBody extends StatelessWidget {
-  const TsunagoBody({
-    Key key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return GridView.builder(
-      itemCount: uchinokoList.length,
-      padding: EdgeInsets.all(kDefaultPadding),
-      shrinkWrap: true,
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        mainAxisSpacing: 10.0,
-        crossAxisSpacing: 10.0,
-        childAspectRatio: 0.8,
-      ),
-      itemBuilder: (context, index) {
-        return GestureDetector(
-          onTap: () {
-            _launchURL(tsunagouList[index].youtubeUrl);
-          },
-          child: Container(
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(kBorderRadius),
-              color: Colors.white,
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey,
-                  offset: Offset(0, 5.0),
-                  blurRadius: 5.0,
-                )
-              ],
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              // TODO: 画像の角を直接丸くするのをやめて、親ウィジェットをに合わせる
-              children: [
-                Center(
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.vertical(
-                      top: Radius.circular(kBorderRadius),
-                    ),
-                    child: Image.asset(
-                      tsunagouList[index].thumbnailPath,
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(kDefaultPadding),
-                  child: Text(
-                    tsunagouList[index].title,
-                    maxLines: 3,
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyle(
-                      fontSize: 14,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
-                  child: Text(
-                    tsunagouList[index].content,
-                    maxLines: 3,
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyle(
-                      fontSize: 12,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        );
-      },
-    );
-  }
-
-  _launchURL(String url) async {
-    if (await canLaunch(url)) {
-      await launch(
-        url,
-        forceSafariVC: true,
-        forceWebView: true,
-      );
-    } else {
-      throw 'Could not launch $url';
-    }
-  }
-}
-
-class OvercomeListView extends StatelessWidget {
-  const OvercomeListView({
-    Key key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      // TODO: 一部のリストはWidgetで作成する
-      shrinkWrap: true,
-      physics: NeverScrollableScrollPhysics(),
-      itemCount: overcomeModelList.length,
-      itemBuilder: (context, index) {
-        return GestureDetector(
-          onTap: () {
-            _launchURL(overcomeModelList[index].url);
-          },
-          child: Container(
-            width: 300,
-            margin: EdgeInsets.only(
-              top: 10,
-              right: 10,
-              left: 10,
-            ),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(kBorderRadius),
-              color: Colors.white,
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey,
-                  offset: Offset(0, 5.0),
-                  blurRadius: 5.0,
-                ),
-              ],
-            ),
-            child: ClipRRect(
-              borderRadius: BorderRadius.all(
-                Radius.circular(kBorderRadius),
-              ),
-              child: Image.asset(
-                overcomeModelList[index].imagePath,
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-        );
-      },
-    );
-  }
-
-  _launchURL(String url) async {
-    if (await canLaunch(url)) {
-      await launch(
-        url,
-        forceSafariVC: true,
-        forceWebView: true,
-      );
-    } else {
-      throw 'Could not launch $url';
-    }
-  }
-}
-
-class TsunagoHeader extends StatelessWidget {
-  const TsunagoHeader({
-    Key key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(
-        top: 10,
-        right: 10,
-        left: 10,
-      ),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(kBorderRadius),
-        color: Colors.pink[400],
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey,
-            offset: Offset(0, 5.0),
-            blurRadius: 5.0,
-          ),
-        ],
-      ),
-      child: Container(
-        margin: EdgeInsets.symmetric(horizontal: 80),
-        color: Colors.white,
-        child: Padding(
-          padding: EdgeInsets.symmetric(
-            vertical: 5,
-            horizontal: 30,
-          ),
-          child: GradientText(
-            "#つなごうみやざき",
-            gradient: LinearGradient(colors: [
-              Colors.pink[400],
-              Colors.yellow[600],
-            ]),
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.w900,
-            ),
-            textAlign: TextAlign.center,
-          ),
-        ),
-      ),
     );
   }
 }
