f49e6859679fbbeb643478ce596146c73386804d
naoyasugita <sugita6878@gmail.com>
2020-11-05 01:27:28 +0900

    ファイルに分離

--- clone_mrt_app/lib/screens/weather/caption_body.dart
+++ clone_mrt_app/lib/screens/weather/caption_body.dart
@@ -1,5 +1,6 @@
 import 'package:clone_mrt_app/models/weather.dart';
 import 'package:clone_mrt_app/constants.dart';
+import 'package:clone_mrt_app/screens/weather/weather_detail.dart';
 import 'package:flutter/material.dart';
 
 class FlexibleCaption extends StatelessWidget {
@@ -56,77 +57,6 @@ class FlexibleCaption extends StatelessWidget {
               blurRadius: 5.0,
             )
           ],
-        ),
-      ),
-    );
-  }
-}
-
-class WeatherDetailView extends StatelessWidget {
-  const WeatherDetailView({
-    Key key,
-    this.index,
-  }) : super(key: key);
-
-  final index;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.lightBlue[50],
-      appBar: AppBar(
-        title: Text('天気'),
-        backgroundColor: kSecondaryColor,
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              padding: EdgeInsets.symmetric(
-                vertical: 10,
-              ),
-              color: Colors.blue[900],
-              child: Center(
-                child: Text(
-                  weatherCattionList.caption[index].subject,
-                  style: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ),
-            Column(
-              children: _buildList(context, index).toList(),
-            ),
-            Container(
-              margin: EdgeInsets.symmetric(
-                vertical: 5,
-              ),
-              child: Text(
-                "Copytight © 1996-2020 XXXXXX Co.,Ltd. All rights reserved.",
-                style: TextStyle(
-                  fontSize: 8,
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  List<Widget> _buildList(BuildContext context, int index) {
-    return List.generate(
-      weatherCattionList.caption[index].imagePath.length,
-      (imageIndex) => Container(
-        margin: EdgeInsets.only(
-          bottom: 5,
-        ),
-        child: Image.asset(
-          weatherCattionList.caption[index].imagePath[imageIndex],
-          fit: BoxFit.cover,
         ),
       ),
     );
